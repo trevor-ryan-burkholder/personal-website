bdead5e3aa313dc64e2eb388fdee2487c8ed6104
Trevor Burkholder <tburkholder@avantia-inc.com>
2026-06-03 13:22:51 -0400

    ADD: images

--- src/content/testimonials.ts
+++ src/content/testimonials.ts
@@ -12,13 +12,13 @@ export const testimonials: Testimonial[] = [
     body: '"Trevor is exceptionally talented and uncommonly motivated. He has a great eye for design, cares about doing things right, and is great at communicating complex ideas with all levels of stakeholders. He understands the full stack and is able to define the needs of the UI developers to the backend developers in a way that moves projects along quickly and gets things done right the first time."',
     name: 'Rex Petersen',
     role: 'DIR. APPLICATION ENGINEERING · WESTERRA CREDIT UNION',
-    image: '../img/rex.jpg',
+    image: '/rex.jpg',
   },
   {
     body: '"Trevor approaches complex issues with a clear, analytical mindset, and consistently comes up with effective solutions. His attention to detail is unmatched — an exceptional ability to identify edge cases and address issues that others might overlook. He is an excellent communicator who can easily explain technical concepts to non-technical stakeholders, and was able to take more junior engineers and mentor them to grow their own skillset."',
     name: "Keith O'Connel",
     role: 'PRACTICE LEAD, DEVOPS & CLOUD · XTIVIA',
-    image: '../img/keith.jpg',
+    image: '/keith.jpg',
   },
 ];
 
